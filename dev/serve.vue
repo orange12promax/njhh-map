1e34d4571d28101aa4f8f903e4ecb854cd1c6760
王晨 <1337245453@qq.com>
2022-09-23 09:18:41 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import NjhhMap from '@/njhh-map.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    NjhhMap
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <njhh-map />
+  </div>
+</template>
